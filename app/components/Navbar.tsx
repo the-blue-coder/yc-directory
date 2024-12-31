d0480cdc55c9abe47f9f2d5300bf187a29337d2d
Jean R. <jd.rakotoarison@gmail.com>
2024-12-31 12:35:18 +0300

    next auth connection with github

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,55 @@
-const Navbar: React.FC = () => {
-    return <div>Navbar</div>;
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar: React.FC = async () => {
+    const session = await auth();
+
+    return (
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+
+                <div className="flex items-center gap-5">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+
+                            <form
+                                action={async () => {
+                                    "use server";
+                                    await signOut();
+                                }}
+                            >
+                                <button type="submit">
+                                    <span>Logout</span>
+                                </button>
+                            </form>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form
+                            action={async () => {
+                                "use server";
+                                await signIn("github");
+                            }}
+                        >
+                            <button type="submit">
+                                <span>Login</span>
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
