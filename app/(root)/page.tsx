a5ea3d717c387e36bd78e2578d2122c15fedf992
Jean R. <jd.rakotoarison@gmail.com>
2024-12-31 13:44:51 +0300

    heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
 const Home: React.FC = () => {
     return (
         <>
-            <h1 className="heading">HOME</h1>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup,
+                    <br /> Connect With Entrepreneurs
+                </h1>
+
+                <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+            </section>
         </>
     );
 };
