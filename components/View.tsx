ffd96aab92a422d39d642f4658f2dda66f738f82
Jean R. <jd.rakotoarison@gmail.com>
2025-01-12 16:06:48 +0300

    views ok

--- components/View.tsx
+++ components/View.tsx
@@ -2,14 +2,18 @@ import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View: React.FC<TViewProps> = async ({ id }) => {
     const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id });
 
-    await writeClient
-        .patch(id)
-        .set({ views: totalViews + 1 })
-        .commit();
+    after(
+        async () =>
+            await writeClient
+                .patch(id)
+                .set({ views: totalViews + 1 })
+                .commit()
+    );
 
     return (
         <div className="view-container">
